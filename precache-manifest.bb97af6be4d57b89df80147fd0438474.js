704c7be814cf45b47886f7cd305293f8ff799efc
sandeep ninja <codewithsandeep140@gmail.com>
2023-08-20 16:48:44 +0530

    Updates

---
+++ precache-manifest.bb97af6be4d57b89df80147fd0438474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "858598a0be0b74ffba86",
+    "url": "/TodoList/static/css/main.c334f5f0.chunk.css"
+  },
+  {
+    "revision": "858598a0be0b74ffba86",
+    "url": "/TodoList/static/js/main.858598a0.chunk.js"
+  },
+  {
+    "revision": "d7f708bf319ae2fd555f",
+    "url": "/TodoList/static/js/1.d7f708bf.chunk.js"
+  },
+  {
+    "revision": "e5fd00049e7ac7cce407",
+    "url": "/TodoList/static/js/runtime~main.e5fd0004.js"
+  },
+  {
+    "revision": "8c227b3471b4409d04dc9dbaf7a2fc5c",
+    "url": "/TodoList/index.html"
+  }
+];
