af7d86c2cde8be182a27113497fce86c260f7b12
SandeepNinja <130206426+SandeepNinja@users.noreply.github.com>
2023-08-25 08:16:14 +0530

    Tasks.js
    Comment added

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,9 +4,11 @@ function Tasks(props) {
   const { tasks, handleChangeTitle, handleDeleteTask, handleCompleted } = props;
   return (
     <div className="tasksContainer">
+    // iterate over each task
       {tasks.length > 0 &&
         tasks.map((task) => {
           return (
+            // individual task
             <Task
               task={task}
               key={task.id}
